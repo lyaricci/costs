a96256eb6de78f3865e9532e59bb89de26b14870
Nathálya Ricci <nathalyaricci@gmail.com>
2023-12-16 15:25:47 -0300

    add app routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,46 @@
+import { Router, Routes, Route } from "react-router-dom"
+
 function App() {
-  return <>Costs</>
+  return (
+    <Router>
+      {/* Navbar */}
+      <Routes>
+        <Route
+          path="/"
+          element={
+            {
+              /* <Home /> */
+            }
+          }
+        />
+        <Route
+          path="/company"
+          element={
+            {
+              /* <Company /> */
+            }
+          }
+        />
+        <Route
+          path="/contact"
+          element={
+            {
+              /* <Contact /> */
+            }
+          }
+        />
+        <Route
+          path="/new-project"
+          element={
+            {
+              /* <NewProject /> */
+            }
+          }
+        />
+      </Routes>
+      {/* Footer */}
+    </Router>
+  )
 }
 
 export default App
