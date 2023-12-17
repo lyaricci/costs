cedb7e69682ad39b19227ed7c483fce538c610b3
Nathálya Ricci <nathalyaricci@gmail.com>
2023-12-17 17:04:35 -0300

    feat: add new project page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 
 import Home from "./pages/Home"
+import NewProject from "./pages/NewProject"
+
 import Container from "./components/layout/Container"
 import Navbar from "./components/layout/Navbar"
 import Footer from "./components/layout/Footer"
@@ -36,14 +38,7 @@ function App() {
               }
             }
           />
-          <Route
-            path="/new-project"
-            element={
-              {
-                /* <NewProject /> */
-              }
-            }
-          />
+          <Route path="/new-project" element={<NewProject />} />
         </Routes>
       </Container>
       <Footer />
---
+++ src/pages/NewProject.jsx
@@ -0,0 +1,11 @@
+import styles from "./newProject.module.css"
+
+export default function NewProject() {
+  return (
+    <div className={styles.newProjectContainer}>
+      <h1>Criar projeto</h1>
+      <p>Crie seu projeto para depois adicionar os serviços</p>
+      {/* <ProjectForm /> */}
+    </div>
+  )
+}
