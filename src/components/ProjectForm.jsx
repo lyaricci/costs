70167f7a1a184f7267370a7dcc16ea024c9237ec
Nathálya Ricci <nathalyaricci@gmail.com>
2023-12-17 20:22:29 -0300

    feat: add api integration

--- src/components/ProjectForm.jsx
+++ src/components/ProjectForm.jsx
@@ -1,3 +1,5 @@
+import { useEffect, useState } from "react"
+
 import Input from "./form/Input"
 import Select from "./form/Select"
 import SubmitButton from "./form/SubmitButton"
@@ -5,6 +7,22 @@ import SubmitButton from "./form/SubmitButton"
 import styles from "./projectForm.module.css"
 
 export default function ProjectForm({ btnText }) {
+  const [categories, setCategories] = useState([])
+
+  useEffect(() => {
+    fetch("http://localhost:3000/categories", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setCategories(data)
+      })
+      .catch((err) => console.log(err))
+  }, [])
+
   return (
     <form className={styles.form}>
       <Input
@@ -19,7 +37,11 @@ export default function ProjectForm({ btnText }) {
         name="budget"
         placeholder="Insira o orçamento total"
       />
-      <Select name="category_id" text="Selecione a categoria" />
+      <Select
+        name="category_id"
+        text="Selecione a categoria"
+        options={categories}
+      />
       <SubmitButton text={btnText} />
     </form>
   )
