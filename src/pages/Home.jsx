2c4316d916778e81c09a1f518523af8bb4c4c0ee
Nathálya Ricci <nathalyaricci@gmail.com>
2023-12-17 00:00:35 -0300

    feat: style homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,15 @@
+import styles from "./home.module.css"
+import savings from "../img/savings.svg"
+
 export default function Home() {
-  return <h1>Home</h1>
+  return (
+    <section className={styles.homeContainer}>
+      <h1>
+        Bem-vindo ao <span>Costs</span>
+      </h1>
+      <p>Comece a gerenciar os seus projetos agora mesmo!</p>
+      <a href="/">Criar projeto</a>
+      <img src={savings} alt="Costs" />
+    </section>
+  )
 }
